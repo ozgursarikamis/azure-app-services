81abcfbdca04857c086a6a64b2168f9cdd16a5d9
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-15 14:32:50 +0300

    update startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace azure_app_services
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
