461c2c3debbc3e8110ed7530da6e823b8a0c2e53
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-15 16:34:10 +0300

    azure script notes

---
+++ Azure/Scripts.ps1
@@ -0,0 +1,15 @@
+Get-AzResourceGroup
+	# lists all resource groups on Azure
+
+ Get-Command *-azappserviceplan
+	#output is: 
+
+	# CommandType     Name                                               Version    Source
+	# -----------     ----                                               -------    ------
+	# Cmdlet          Get-AzAppServicePlan                               1.11.0     Az.Websites
+	# Cmdlet          New-AzAppServicePlan                               1.11.0     Az.Websites
+	# Cmdlet          Remove-AzAppServicePlan                            1.11.0     Az.Websites
+	# Cmdlet          Set-AzAppServicePlan                               1.11.0     Az.Websites
+	
+ Get-Help New-AzAppServicePlan
+	#Gets all help items about New-AzAppServicePlan
